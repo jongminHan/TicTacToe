e24524feb76e09151f6ee827eaa2f1b66651df54
Jongmin Han <33759102+jongminHan@users.noreply.github.com>
2019-09-26 17:36:33 +0900

    Add Game class

---
+++ Game.cpp
@@ -0,0 +1,48 @@
+#include "Game.h"
+
+
+namespace lecture
+{
+	Game::Game(int width, int height, std::string title)
+	{
+		mData->window.create(sf::VideoMode(width, height), title, sf::Style::Close | sf::Style::TitleBar);
+
+		Run();
+	}
+
+	void Game::Run()
+	{
+		float newTime;
+		float frameTime;
+		float interpolation;
+
+		float currentTime = mClock.getElapsedTime().asSeconds();
+		float accumulator = 0.0f;
+
+		while (mData->window.isOpen())
+		{
+			mData->machine.ProcessStateChanges();
+			newTime = mClock.getElapsedTime().asSeconds();
+			frameTime = newTime - currentTime;
+
+			if (frameTime > 0.25f)
+			{
+				frameTime = 0.25f;
+			}
+
+			currentTime = newTime;
+			accumulator += frameTime;
+
+			while (accumulator >= dt)
+			{
+				mData->machine.GetActiveState()->HandleInput();
+				mData->machine.GetActiveState()->Update(dt);
+
+				accumulator -= dt;
+			}
+
+			interpolation = accumulator / dt;
+			mData->machine.GetActiveState()->Draw(interpolation);
+		}
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Game.h"
 
 int main()
 {
+	lecture::Game(800, 600, "SFML Starter");
 	return EXIT_SUCCESS;
 }
