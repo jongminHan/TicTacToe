c748d012733c205c7dbc87ebd02233f464d8600f
Jongmin Han <33759102+jongminHan@users.noreply.github.com>
2019-09-27 09:02:56 +0900

    Add Splash State

---
+++ DEFINITIONS.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define SCRREN_WIDTH 768
+#define SCREEN_HEIGHT 1136
+
+#define SPLASH_STATE_SHOW_TIME 0.5
+
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resource Files/res/Splash Background.png"
+
+
+
