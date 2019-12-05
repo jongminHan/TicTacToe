c29494a2fdad16ecaad7685a76b5eeca10791da4
jongminHan <jongmin.han17@gmail.com>
2019-12-05 21:32:50 +0900

    Add AI class

---
+++ AI.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <array>
+#include "Game.h"
+
+namespace lecture
+{
+	class AI
+	{
+	public:
+		AI(int playerPiece, GameDataRef data);
+		void PlacePiece(int(*gridArray)[3][3], sf::Sprite gridPieces[3][3], int* gameState);
+
+	private:
+		void checkSection(int x1, int y1, int x2, int y2, int X, int Y, int pieceToCheck, int(*gridArray)[3][3], sf::Sprite gridPieces[3][3]);
+		void checkIfPieceIsEmpty(int X, int Y, int pieceToCheck, int(*gridArray)[3][3], sf::Sprite gridPieces[3][3]);
+
+		int mAiPiece;
+		int mPlayerPiece;
+
+		std::vector<std::array<int, 6>> mCheckMatchVector;
+	};
+}
